d9c41c0e6525b4bf2210c9a008ca8d5c14969912
Manuel Scholz <manuel.serrano943@gmail.com>
2025-07-27 19:36:25 +0200

    Feat: SafeAreaView para tabs

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -5,6 +5,7 @@ import { useFonts } from "expo-font";
 import { colors } from "../../src/utils/styles";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { initDb } from "../../src/utils/sqlite";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 SplashScreen.preventAutoHideAsync();
 export default function Layout() {
@@ -33,61 +34,70 @@ export default function Layout() {
 
     return (
         <View style={styles.container}>
-            {/* <Stack.Screen options={{ headerShown: false }} /> */}
-            <Tabs backBehavior="history" options={{ headerShown: false }}>
-                <Tabs.Screen
-                    name="index"
-                    options={{
-                        tabBarStyle: { height: 60, backgroundColor: colors.primary },
-                        tabBarLabel: "Traductor",
-                        tabBarLabelStyle: { marginTop: 2 },
-                        tabBarIcon: ({ focused }) => <MaterialIcons name="translate" size={25} color={focused ? colors.accent : "#fff"} />,
-                        tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
-                        headerShown: false,
-                        tabBarActiveTintColor: colors.accent,
-                        tabBarInactiveTintColor: "#fff",
-                    }}
-                />
-                <Tabs.Screen
-                    name="learn"
-                    options={{
-                        tabBarStyle: { height: 60, backgroundColor: colors.primary },
-                        tabBarLabel: "Aprender",
-                        tabBarLabelStyle: { marginTop: 2 },
-                        tabBarIcon: ({ focused }) => <MaterialIcons name="wysiwyg" size={25} color={focused ? colors.accent : "#fff"} />,
-                        tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
-                        headerShown: false,
-                        tabBarActiveTintColor: colors.accent,
-                        tabBarInactiveTintColor: "#fff",
-                    }}
-                />
-                <Tabs.Screen
-                    name="send"
-                    options={{
-                        tabBarStyle: { height: 60, backgroundColor: colors.primary },
-                        tabBarLabel: "Envíar código",
-                        tabBarLabelStyle: { marginTop: 2 },
-                        tabBarIcon: ({ focused }) => <MaterialIcons name="send" size={25} color={focused ? colors.accent : "#fff"} />,
-                        tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
-                        headerShown: false,
-                        tabBarActiveTintColor: colors.accent,
-                        tabBarInactiveTintColor: "#fff",
-                    }}
-                />
-                <Tabs.Screen
-                    name="alphabet"
-                    options={{
-                        tabBarStyle: { height: 60, backgroundColor: colors.primary },
-                        tabBarLabel: "Abecedario",
-                        tabBarLabelStyle: { marginTop: 2 },
-                        tabBarIcon: ({ focused }) => <MaterialIcons name="book" size={25} color={focused ? colors.accent : "#fff"} />,
-                        tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
-                        headerShown: false,
-                        tabBarActiveTintColor: colors.accent,
-                        tabBarInactiveTintColor: "#fff",
-                    }}
-                />
-            </Tabs>
+            <SafeAreaProvider>
+
+                <SafeAreaView style={styles.container}>
+
+                    <Tabs
+                        backBehavior="history"
+                        options={{ headerShown: false }}
+                    >
+                        <Tabs.Screen
+                            name="index"
+                            options={{
+                                tabBarStyle: { height: 60, backgroundColor: colors.primary },
+                                tabBarLabel: "Traductor",
+                                tabBarLabelStyle: { marginTop: 2 },
+                                tabBarIcon: ({ focused }) => <MaterialIcons name="translate" size={25} color={focused ? colors.accent : "#fff"} />,
+                                tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
+                                headerShown: false,
+                                tabBarActiveTintColor: colors.accent,
+                                tabBarInactiveTintColor: "#fff",
+                            }}
+                        />
+                        <Tabs.Screen
+                            name="learn"
+                            options={{
+                                tabBarStyle: { height: 60, backgroundColor: colors.primary },
+                                tabBarLabel: "Aprender",
+                                tabBarLabelStyle: { marginTop: 2 },
+                                tabBarIcon: ({ focused }) => <MaterialIcons name="wysiwyg" size={25} color={focused ? colors.accent : "#fff"} />,
+                                tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
+                                headerShown: false,
+                                tabBarActiveTintColor: colors.accent,
+                                tabBarInactiveTintColor: "#fff",
+                            }}
+                        />
+                        <Tabs.Screen
+                            name="send"
+                            options={{
+                                tabBarStyle: { height: 60, backgroundColor: colors.primary },
+                                tabBarLabel: "Envíar código",
+                                tabBarLabelStyle: { marginTop: 2 },
+                                tabBarIcon: ({ focused }) => <MaterialIcons name="send" size={25} color={focused ? colors.accent : "#fff"} />,
+                                tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
+                                headerShown: false,
+                                tabBarActiveTintColor: colors.accent,
+                                tabBarInactiveTintColor: "#fff",
+                            }}
+                        />
+                        <Tabs.Screen
+                            name="alphabet"
+                            options={{
+
+                                tabBarStyle: { height: 60, backgroundColor: colors.primary },
+                                tabBarLabel: "Abecedario",
+                                tabBarLabelStyle: { marginTop: 2 },
+                                tabBarIcon: ({ focused }) => <MaterialIcons name="book" size={25} color={focused ? colors.accent : "#fff"} />,
+                                tabBarBadgeStyle: { color: "#fff", backgroundColor: "#337AB7" },
+                                headerShown: false,
+                                tabBarActiveTintColor: colors.accent,
+                                tabBarInactiveTintColor: "#fff",
+                            }}
+                        />
+                    </Tabs>
+                </SafeAreaView>
+            </SafeAreaProvider>
             <StatusBar style="light" />
         </View>
     )
@@ -95,5 +105,7 @@ export default function Layout() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        position: "relative",
+        justifyContent: "center",
     },
 })
